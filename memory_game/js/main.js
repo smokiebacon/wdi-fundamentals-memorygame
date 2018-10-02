d90f0898dbf37922887405505dfeecc1451daa32
Abraham Hsu <smokiebacon@gmail.com>
2018-10-01 22:14:33 -0700

    created functions, game logic added

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,27 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsinPlay = [];
+
+
+function checkMatch() {
+  if (cardsinPlay.length == 2) {
+            if (cardsinPlay[0] === cardsinPlay[1]) {
+              alert("Match found!"); }
+            else {
+              alert("Match not found. Try Again");
+            }
+          }
+}
+
+function flipCard (cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsinPlay.push(cards[cardId]);
+checkMatch();
+}
+
+
+
 let cardOne = cards[0];
 let cardTwo = cards[2];
-
-cardsinPlay.push(cardOne);
-cardsinPlay.push(cardTwo);
 console.log(cardsinPlay);
-
-if (cardsinPlay.length == 2) {
-        if (cardsinPlay[0] === cardsinPlay[1]) {
-          alert("Match found!"); }
-        else {
-          alert("Match not found. Try Again");
-        }
-      }
+flipCard (0);
+flipCard (1);
